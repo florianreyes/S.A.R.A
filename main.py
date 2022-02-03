57174e1d8025066f87210b479b5dafdedbdf025f
Florian <Dominique@Florian.fibertel.com.ar>
2022-02-03 13:11:51 -0300

    added requirements and time function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
+from datetime import datetime
 import time
 import speech_recognition as sr
 
@@ -9,6 +10,12 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     # print("Hello sir, how can i help you?")
     audio = r.listen(source)
+
+    #  Recognizes audio using google
     text = r.recognize_google(audio)
 
-    print(text)
+    # Added function to return time when asked
+    if str(text).lower() == 'what time is it':
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(current_time)
