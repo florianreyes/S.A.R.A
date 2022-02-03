bdf7057cbd30a39d76d9e1f7563dff568bc00520
Florian <Dominique@Florian.fibertel.com.ar>
2022-02-03 16:33:21 -0300

    functioning version, gonna try including wolframalpha

--- main.py
+++ main.py
@@ -21,29 +21,16 @@ def get_audio():
     with sr.Microphone() as source:
         audio = r.listen(source)
         said = ""
-
         try:
-            said = r.recognize_google(audio)  # Recognizes audio using google
+            said = r.recognize_google(audio)
+            print(said)
         except Exception as e:
             print("Exception: " + str(e))
     return said
 
 
-speak("hello")
-print(get_audio())
-
-
-# Get audio from the microphone
-
-# r = sr.Recognizer()
-# with sr.Microphone() as source:
-#     # print("Hello sir, how can i help you?")
-#     audio = r.listen(source)
-#     text = r.recognize_google(audio)  # Recognizes audio using google
-# print(text)
-# # Added function to return time when asked
-
-# if str(text).lower() == 'what time is it':
-#     t = time.localtime()
-#     current_time = time.strftime("%H:%M:%S", t)
-#     print(current_time)
+text = get_audio()
+if "hello" in text:
+    speak("Hi, how are you sir?")
+if "fine" in text:
+    speak("Excellent, How can i help you today?")
